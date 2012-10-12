cb6105f837c4d5ca3cd06306a7b978905e89cb40
Jérôme Schneider <jschneider@entrouvert.com>
2012-10-12 17:36:11 +0200

    Add a script to export Flaure DB

---
+++ scripts/export_db.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+
+from datetime import datetime
+
+# Config
+dbs = ["F_ST_ETIENNE_CMPP", "F_ST_ETIENNE_CAMSP", "F_ST_ETIENNE_SESSAD", "F_ST_ETIENNE_SESSAD_TED"]
+tables = ["actes", "actes_non_factures", "ev"]
+export_path = os.path.join("C:\\", "export", datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+
+os.mkdir(export_path)
+
+for db in dbs:
+    db_dir = os.path.join(export_path, db)
+    os.mkdir(db_dir)
+    for table in tables:
+        os.system('sqlcmd -S SRVAPS -E -h-1 -s, -Q"SET NOCOUNT ON;' + \
+                'DECLARE @colnames VARCHAR(max); ' + \
+                "SELECT @colnames = COALESCE(@colnames + ',', '') " + \
+                '+ column_name from %s.INFORMATION_SCHEMA.COLUMNS ' % db + \
+                " where TABLE_NAME='%s'; select @colnames;\" " % table + \
+                '-o "%s-title.csv"' % os.path.join(db_dir, table))
+        os.system('bcp %s.dbo.%s out %s-data.csv -c -t, -T -SSRVAPS' % (db, table, os.path.join(db_dir, table)))
+        title = open("%s-title.csv" % os.path.join(db_dir, table), 'r')
+        data = open("%s-data.csv" % os.path.join(db_dir, table), 'r')
+        res = open("%s.csv" % os.path.join(db_dir, table), "a+")
+        res.write(title.readline())
+        res.write(data.read())
+        title.close()
+        data.close()
+        res.close()
+        os.remove("%s-title.csv" % os.path.join(db_dir, table))
+        os.remove("%s-data.csv" % os.path.join(db_dir, table))
+
+
