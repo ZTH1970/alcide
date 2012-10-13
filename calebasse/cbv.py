5e0f0c153e0c81ac265d9076ecf59e93e2500317
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2012-10-13 16:35:01 +0200

    add subclasses of generic class based views for global template variables

---
+++ calebasse/cbv.py
@@ -0,0 +1,27 @@
+from django.views.generic import list as list_cbv, edit, base # ListView
+# from django.views.generic.edit import # CreateView, DeleteView, UpdateView
+
+from views import SERVICES_MAP
+
+
+class ServiceViewMixin(object):
+    def get_context_data(self, **kwargs):
+        context = super(ServiceViewMixin, self).get_context_data(**kwargs)
+        context['service'] = self.kwargs.get('service')
+        context['service_name'] = SERVICES_MAP.get(context['service'])
+        return context
+
+class TemplateView(ServiceViewMixin, base.TemplateView):
+    pass
+
+class ListView(ServiceViewMixin, list_cbv.ListView):
+    pass
+
+class CreateView(ServiceViewMixin, edit.CreateView):
+    pass
+
+class DeleteView(ServiceViewMixin, edit.DeleteView):
+    pass
+
+class UpdateView(ServiceViewMixin, edit.UpdateView):
+    pass
