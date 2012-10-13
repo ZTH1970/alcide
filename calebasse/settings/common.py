b9390ab1aa1494ecf35c42d5dceb0a5120b32823
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2012-10-13 16:44:09 +0200

    add the request context processor

--- calebasse/settings/common.py
+++ calebasse/settings/common.py
@@ -118,6 +118,15 @@ TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, "templates")
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.core.context_processors.request",
+    "django.contrib.messages.context_processors.messages")
+
 AUTH_PROFILE_MODULE = "cale_base.CalebasseUser"
 
 INSTALLED_APPS = (
