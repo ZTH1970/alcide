87018c8eb5370922f409bd333f6f88f1e61642e0
Jérôme Schneider <jschneider@entrouvert.com>
2012-10-15 18:20:03 +0200

    [Models] Add schools and oranizations

---
+++ calebasse/cale_base/models/organization.py
@@ -0,0 +1,46 @@
+
+from django.db import models
+
+class Place(models.Model):
+
+    class Meta:
+        app_label = 'cale_base'
+
+    def __unicode__(self):
+        return self.name
+
+    name = models.CharField(max_length=40, blank=Flase)
+    address = models.CharField(max_length=120)
+    address_complement = models.CharField(max_length=120, blank=True, null=True, default=None)
+    zip_code = models.IntegerField(max_length=6)
+    city = models.CharField(max_length=80)
+
+class OrganizationAnnex(models.Model):
+
+    class Meta:
+        app_label = 'cale_base'
+
+    def __unicode__(self):
+        return self.name
+
+    name = models.CharField(max_length=40, blank=Flase)
+    phone = models.CharField(max_length=30)
+    fax = models.CharField(max_length=30)
+    email = models.EmailField()
+    services = models.ForeignKey('Service')
+
+class Organization(models.Model):
+
+    class Meta:
+        app_label = 'cale_base'
+
+    def __unicode__(self):
+        return self.name
+
+    name = models.CharField(max_length=40, blank=Flase)
+    phone = models.CharField(max_length=30)
+    fax = models.CharField(max_length=30)
+    email = models.EmailField()
+    services = models.ForeignKey('Service')
+    # TODO: add this fields : finess, suite, dm, dpa, genre, categorie, statut_juridique, mft, mt, dmt
+
--- calebasse/cale_base/models/transport.py
+++ calebasse/cale_base/models/transport.py
@@ -15,16 +15,24 @@ class TransportCompany(models.Model):
     class Meta:
         app_label = 'cale_base'
 
+    def __unicode__(self):
+        return self.name
+
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
+    zip_code = models.IntegerField(max_length=6)
     city = models.CharField(max_length=80)
     phone = models.CharField(max_length=25)
-
+    email = models.EmailField()
+    contact_name = models.CharField(max_length=80)
 
 class TransportType(models.Model):
 
     class Meta:
         app_label = 'cale_base'
+
+    def __unicode__(self):
+        return self.name
 
     name = models.CharField(max_length=100)
 
