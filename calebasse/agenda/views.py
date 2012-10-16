0db45b73beb9fc95dab59a0309a81f5fb734897f
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2012-10-16 23:52:38 +0200

    add therapeutes and personnel list to the agenda template context datas

--- calebasse/agenda/views.py
+++ calebasse/agenda/views.py
@@ -2,7 +2,19 @@ import datetime
 
 from django.shortcuts import redirect
 
+from calebasse.cbv import TemplateView
+from calebasse.personnes.models import Therapeute, Personnel
+
 def redirect_today(request, service):
     '''If not date is given we redirect on the agenda for today'''
     return redirect('agenda', date=datetime.date.today().strftime('%Y-%m-%d'),
             service=service)
+
+class AgendaHomepageView(TemplateView):
+    template_name = 'agenda/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AgendaHomepageView, self).get_context_data(**kwargs)
+        context['therapeutes'] = Therapeute.objects.for_service(self.service)
+        context['personnels'] = Personnel.objects.for_service(self.service)
+        return context
