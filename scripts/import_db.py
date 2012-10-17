1c9340b5b703cc31d1b6afd1dfa5c2ddf48eb67a
Jérôme Schneider <jschneider@entrouvert.com>
2012-10-17 17:47:19 +0200

    Import databases: add a first draft

---
+++ scripts/import_db.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import ipdb
+import os
+import csv
+
+from datetime import datetime
+
+import calebasse.settings
+import django.core.management
+
+django.core.management.setup_environ(calebasse.settings)
+
+# Config
+db_path = "/home/jschneider/dav/temp/20121015-120922"
+
+dbs = ["F_ST_ETIENNE_CMPP", "F_ST_ETIENNE_CAMSP", "F_ST_ETIENNE_SESSAD", "F_ST_ETIENNE_SESSAD_TED"]
+tables = ["notes", "ev"]
+
+def _get_dict(cols, line):
+    """"""
+    res = {}
+    for i, data in enumerate(line):
+        res[cols[i]] = data
+    return res
+
+
+def ev_mapper(data):
+    """ """
+    pass
+
+def notes_mapper(data, note):
+    """ """
+    pass
+
+mappers = {
+        "ev": ev_mapper
+        "notes": notes_mapper
+        }
+
+def main():
+    """ """
+    for db in dbs:
+        for table in tables:
+            csvfile = open(os.path.join(db_path, db, '%s.csv' % table), 'rb')
+            csvlines = csv.reader(csvfile, delimiter=',', quotechar='"')
+            cols = csvlines.next()
+            for line in csvlines:
+                data = _get_dict(cols, line)
+                mappers[table](data)
+            csvfile.close()
+
+
+if __name__ == "__main__":
+    main()
+
