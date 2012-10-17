6351faa746924920acebeac8d78261932ed745a5
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2012-10-17 16:39:48 +0200

    add model field for weekday, add BaseModelMixin for a default __repr__() implementation

--- calebasse/models.py
+++ calebasse/models.py
@@ -1,5 +1,13 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 from django.contrib.localflavor.fr.forms import FRPhoneNumberField, FRZipCodeField
+
+WEEKDAYS = (u'lundi', u'mardi', u'mercredi', u'jeudi', u'vendredi')
+
+class BaseModelMixin(object):
+    def __repr__(self):
+        return '<%s %s %r>' % (self.__class__.__name__, self.id, unicode(self))
 
 
 class PhoneNumberField(models.CharField):
@@ -12,6 +20,7 @@ class PhoneNumberField(models.CharField):
         default.update(kwargs)
         return super(PhoneNumberField, self).formfield(**default)
 
+
 class ZipCodeField(models.CharField):
     def __init__(self, **kwargs):
         kwargs['max_length'] = 5
@@ -21,3 +30,14 @@ class ZipCodeField(models.CharField):
         default = { 'form_class': FRZipCodeField }
         default.update(kwargs)
         return super(ZipCodeField, self).formfield(**default)
+
+
+
+class WeekdayField(models.CharField):
+    WEEKDAYS_CHOICE = ((None, u'Aucun'),) \
+            + tuple(zip(WEEKDAYS, map(unicode.title, WEEKDAYS)))
+
+    def __init__(self, **kwargs):
+        kwargs['max_length'] = 16
+        kwargs['choices'] = self.WEEKDAYS_CHOICE
+        super(WeekdayField, self).__init__(**kwargs)
