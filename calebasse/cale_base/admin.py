74c1e580fcd356a09bf86210c1249a074503487f
Jérôme Schneider <jschneider@entrouvert.com>
2012-10-19 09:22:31 +0200

    Initial data with peoples

--- calebasse/cale_base/admin.py
+++ calebasse/cale_base/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 
 from calebasse.actes.models import Act, EventAct
 from calebasse.agenda.models import Event
-from calebasse.personnes.models import Worker
+from calebasse.personnes.models import Worker, TimeTable
 from calebasse.ressources.models import TransportCompany, WorkerType, Service, Office, School, Room
 
 admin.site.register(Act)
 admin.site.register(EventAct)
 admin.site.register(Event)
 admin.site.register(Worker)
+admin.site.register(TimeTable)
 admin.site.register(TransportCompany)
 admin.site.register(WorkerType)
 admin.site.register(Service)
