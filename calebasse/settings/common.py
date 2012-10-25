c9736177c4aa7ee048be56a3cbf48885cad0c390
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2012-10-25 11:08:10 +0200

    move static files in a directory at the project root

--- calebasse/settings/common.py
+++ calebasse/settings/common.py
@@ -7,7 +7,6 @@ from ..settings import PROJECT_PATH
 
 DEBUG = True
 TEMPLATE_DEBUG = True
-PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '..')
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -66,7 +65,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(os.path.join(PROJECT_PATH, '..'), 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -77,6 +76,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -116,7 +116,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, "templates")
+    os.path.join(PROJECT_PATH, "templates")
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
