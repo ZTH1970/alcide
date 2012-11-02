2a928c61244602607151ac86a493e280a91bb758
Mikaël Ates <mates@entrouvert.com>
2012-11-02 06:25:33 +0100

    dossiers: create FileState model.

--- calebasse/dossiers/models.py
+++ calebasse/dossiers/models.py
@@ -11,6 +11,40 @@ from calebasse.ressources.models import ServiceLinkedAbstractModel
 logger = logging.getLogger('calebasse.dossiers')
 
 
+class FileState(models.Model):
+
+    class Meta:
+        app_label = 'dossiers'
+
+    patient = models.ForeignKey('dossiers.PatientRecord',
+        verbose_name=u'Dossier patient', editable=False)
+    state_name = models.CharField(max_length=150)
+    created = models.DateTimeField(u'Création', auto_now_add=True)
+    date_selected = models.DateTimeField()
+    author = \
+        models.ForeignKey(User,
+        verbose_name=u'Auteur', editable=False)
+    comment = models.TextField(max_length=3000, blank=True, null=True)
+    previous_state = models.ForeignKey('FileState',
+        verbose_name=u'Etat précédent',
+        editable=False, blank=True, null=True)
+
+    def get_next_state(self):
+        try:
+            return FileState.objects.get(previous_state=self)
+        except:
+            return None
+
+    def save(self, **kwargs):
+        self.date_selected = \
+            datetime(self.date_selected.year,
+                self.date_selected.month, self.date_selected.day)
+        super(FileState, self).save(**kwargs)
+
+    def __unicode__(self):
+        return self.state_name + ' ' + str(self.date_selected)
+
+
 class PatientRecord(ServiceLinkedAbstractModel, People):
     class Meta:
         verbose_name = u'Dossier'
