8b82d1faaceddbb4eb7d2b46b396d7e7023de5ca
Mikaël Ates <mates@entrouvert.com>
2012-11-02 06:34:10 +0100

    dossiers: add FileRecord creation and state switching tests.

--- calebasse/dossiers/tests.py
+++ calebasse/dossiers/tests.py
@@ -1,16 +1,18 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+# -*- coding: utf-8 -*-
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+from models import create_patient
+from calebasse.ressources.models import Service
+from states import *
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class PatientRecordTest(TestCase):
+
+    def test_states(self):
+        creator = User.objects.create(username='John')
+        for service_name in ('CMPP', 'CAMSP', 'SESSAD'):
+            service = Service.objects.create(name=service_name)
+            patient = create_patient('John', 'Doe', service, creator)
+            for state in STATES[service.name].keys()[1:]:
+                patient.set_state(state, creator)
