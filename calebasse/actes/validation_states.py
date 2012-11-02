7df5244db5bf955bda328beafa86b317a0946991
Mikaël Ates <mates@entrouvert.com>
2012-11-02 06:38:22 +0100

    actes: define validation states.

---
+++ calebasse/actes/validation_states.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+'''
+    The state name is for instance the string 'NON_VALIDE'
+    The value given in the dictionnary can only be used for display.
+'''
+
+NON_VALIDE = u'Non validé'
+VALIDE = u'Validé'
+ABS_NON_EXC = u'Absence non excusée'
+ABS_EXC = u'Absence excusée'
+ANNUL_NOUS = u'Annulé par nous'
+ANNUL_FAMILLE = u'Annulé par la famille'
+ABS_ESS_PPS = u'Absence ESS PPS'
+ENF_HOSP = u'Enfant hospitalisé'
+ACT_DOUBLE = u'Acte en double'
+
+VALIDATION_STATES = {'NON_VALIDE': NON_VALIDE,
+            'VALIDE': VALIDE,
+            'ABS_NON_EXC': ABS_NON_EXC,
+            'ABS_EXC': ABS_EXC,
+            'ANNUL_NOUS': ANNUL_NOUS,
+            'ANNUL_FAMILLE': ANNUL_FAMILLE,
+            'ABS_ESS_PPS': ABS_ESS_PPS,
+            'ENF_HOSP': ENF_HOSP,
+            'ACT_DOUBLE': ACT_DOUBLE}
