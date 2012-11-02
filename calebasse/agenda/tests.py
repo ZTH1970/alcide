2619e0975149c43ea58f4b62e30449973dfb6a90
Mikaël Ates <mates@entrouvert.com>
2012-11-02 07:37:18 +0100

    agenda: update tests.

--- calebasse/agenda/tests.py
+++ calebasse/agenda/tests.py
@@ -4,6 +4,7 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+from datetime import datetime
 
 from django.test import TestCase
 from django.contrib.auth.models import User
@@ -13,10 +14,13 @@ from dateutil import rrule
 from calebasse.agenda.models import Occurrence, Event
 from calebasse.actes.models import EventAct
 from calebasse.dossiers.models import PatientRecord
+from calebasse.dossiers.models import create_patient
 from calebasse.ressources.models import ActType, Service, WorkerType
 from calebasse.personnes.models import Worker
 
 class EventTest(TestCase):
+    def setUp(self):
+        self.creator = User.objects.create(username='John')
 
     def test_create_events(self):
         event = Event.objects.create_event("test 1", 'un test', description='42 42',
@@ -25,23 +29,25 @@ class EventTest(TestCase):
         self.assertEqual(str(event), 'test 1')
 
     def test_create_appointments(self):
-        patient = PatientRecord.objects.create(first_name='Jean', last_name='Le Pouple')
+        service_camsp = Service.objects.create(name='CAMSP')
+
+        patient = create_patient('Jean', 'Lepoulpe', service_camsp, self.creator, date_selected=datetime(2020, 10, 5))
         wtype = WorkerType.objects.create(name='ElDoctor', intervene=True)
         therapist1 = Worker.objects.create(first_name='Bob', last_name='Leponge', type=wtype)
         therapist2 = Worker.objects.create(first_name='Jean', last_name='Valjean', type=wtype)
         therapist3 = Worker.objects.create(first_name='Pierre', last_name='PaulJacques', type=wtype)
         act_type = ActType.objects.create(name='trepanation')
         service = Service.objects.create(name='CMPP')
-        act_event = EventAct.objects.create_patient_appointment('RDV avec M X', patient,
+        act_event = EventAct.objects.create_patient_appointment(self.creator, 'RDV avec M X', patient,
                 [therapist1, therapist2], act_type, service,
                 start_datetime=datetime(2020, 10, 2, 7, 15),
                 end_datetime=datetime(2020, 10, 2, 9, 20),
                 freq=rrule.WEEKLY, byweekday=rrule.FR, until=datetime(2040, 10, 2))
-        act_event2 = EventAct.objects.create_patient_appointment('RDV avec M Y', patient, [therapist3],
+        act_event2 = EventAct.objects.create_patient_appointment(self.creator, 'RDV avec M Y', patient, [therapist3],
                 act_type, service, start_datetime=datetime(2020, 10, 2, 10, 15),
                 end_datetime=datetime(2020, 10, 2, 12, 20),
                 freq=rrule.WEEKLY, byweekday=rrule.FR, until=datetime(2021, 10, 2))
-        self.assertEqual(str(act_event), 'RDV avec M X')
+        self.assertEqual(str(act_event), 'Rdv le 2020-10-02 07:15:00 de Jean Lepoulpe avec Bob Leponge, Jean Valjean pour trepanation (1)')
         self.assertEqual(
                 str(Occurrence.objects.daily_occurrences(datetime(2020, 10, 8), [therapist2])),
                 '[]'
@@ -64,4 +70,3 @@ class EventTest(TestCase):
         self.assertEqual(str(event), 'Conge')
         #event = user.event.occurrence_set.all()[0]
         #self.assertEqual(event.end_time - event.start_time, timedelta(0, 7200))
-
