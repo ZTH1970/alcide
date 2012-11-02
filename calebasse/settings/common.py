49507b498db3e591d0d74146f2cd872bd504af98
Mikaël Ates <mates@entrouvert.com>
2012-11-02 08:58:45 +0100

    middleware: Create a middleware to grab the request.

--- calebasse/settings/common.py
+++ calebasse/settings/common.py
@@ -104,6 +104,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'calebasse.middleware.request.GlobalRequestMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -154,6 +155,7 @@ INSTALLED_APPS = (
     'calebasse.facturation',
     'calebasse.personnes',
     'calebasse.ressources',
+    'calebasse.middleware.request',
 )
 
 INTERNAL_IPS=('127.0.0.1',)
