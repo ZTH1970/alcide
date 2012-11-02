6163f2ff9a9518d10eb7249caa7d32d9a87a9b41
Mikaël Ates <mates@entrouvert.com>
2012-11-02 06:21:20 +0100

    dossiers: add creation fields to PatientRecord model.

--- calebasse/dossiers/models.py
+++ calebasse/dossiers/models.py
@@ -3,6 +3,7 @@
 import logging
 
 from django.db import models
+from django.contrib.auth.models import User
 
 from calebasse.personnes.models import People
 from calebasse.ressources.models import ServiceLinkedAbstractModel
@@ -15,5 +16,10 @@ class PatientRecord(ServiceLinkedAbstractModel, People):
         verbose_name = u'Dossier'
         verbose_name_plural = u'Dossiers'
 
+    created = models.DateTimeField(u'création', auto_now_add=True)
+    creator = \
+        models.ForeignKey(User,
+        verbose_name=u'Créateur dossier patient',
+        editable=False)
     contacts = models.ManyToManyField('personnes.People',
             related_name='contact_of')
