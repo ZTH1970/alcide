9389577696e5caf14be922c053dbbf64baef2e94
Mikaël Ates <mates@entrouvert.com>
2012-11-06 09:54:24 +0100

    actes: choose to commit validation or not and return more stats.

--- calebasse/actes/validation.py
+++ calebasse/actes/validation.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import datetime
 
+from django.db import transaction
+
 
 def get_acts_of_the_day(date):
     from models import EventAct
@@ -50,17 +52,37 @@ def get_days_with_all_acts_locked(start_day, end_day):
     return result
 
 
-def automated_validation(date, service, user):
+def automated_validation(date, service, user, commit=True):
     nb_acts_double = 0
     nb_acts_validated = 0
-    nb_acts_absence = 0
+    nb_acts_abs_non_exc = 0
+    nb_acts_abs_exc = 0
+    nb_acts_annul_nous = 0
+    nb_acts_annul_famille = 0
+    nb_acts_abs_ess_pps = 0
+    nb_acts_enf_hosp = 0
     acts_of_the_day = get_acts_of_the_day(date)
+    for act in acts_of_the_day:
+        if act.is_state('ABS_NON_EXC'):
+            nb_acts_abs_non_exc = nb_acts_abs_non_exc + 1
+        if act.is_state('ABS_EXC'):
+            nb_acts_abs_exc = nb_acts_abs_exc + 1
+        if act.is_state('ANNUL_NOUS'):
+            nb_acts_annul_nous = nb_acts_annul_nous + 1
+        if act.is_state('ANNUL_FAMILLE'):
+            nb_acts_annul_famille = nb_acts_annul_famille + 1
+        if act.is_state('ABS_ESS_PPS'):
+            nb_acts_abs_ess_pps = nb_acts_abs_ess_pps + 1
+        if act.is_state('ENF_HOSP'):
+            nb_acts_enf_hosp = nb_acts_enf_hosp + 1
+
     nb_acts_total = len(acts_of_the_day)
     patients = {}
     if service.name == 'CMPP':
         # Verification des actes en doubles
         acts = [act for act in acts_of_the_day \
-            if act.get_state().state_name in ('VALIDE', 'NON_VALIDE')]
+            if act.get_state().state_name in ('VALIDE', 'NON_VALIDE',
+                'ACT_DOUBLE')]
         for act in acts:
             if act.patient not in patients:
                 patients[act.patient] = []
@@ -73,29 +95,35 @@ def automated_validation(date, service, user):
                 found_one = False
                 for act in acts:
                     if not found_one:
-                        if not act.is_state('VALIDE'):
+                        if not act.is_state('VALIDE') and commit:
                             act.set_state('VALIDE', author=user, auto=True)
                         found_one = True
                         nb_acts_validated = nb_acts_validated + 1
                     else:
-                        act.set_state('ACT_DOUBLE', author=user, auto=True)
+                        if commit:
+                            act.set_state('ACT_DOUBLE', author=user, auto=True)
                         nb_acts_double = nb_acts_double + 1
             else:
-                if acts[0].is_state('NON_VALIDE'):
+                if acts[0].is_state('NON_VALIDE') and commit:
                     acts[0].set_state('VALIDE', author=user, auto=True)
-                    nb_acts_validated = nb_acts_validated + 1
+                nb_acts_validated = nb_acts_validated + 1
     else:
         acts = [act for act in acts_of_the_day if act.is_state('NON_VALIDE')]
         for act in acts:
-            act.set_state('VALIDE', author=user, auto=True)
+            if commit:
+                act.set_state('VALIDE', author=user, auto=True)
             nb_acts_validated = nb_acts_validated + 1
-    nb_acts_absence = nb_acts_total - nb_acts_validated - nb_acts_double
+
     # Acts locking
     for act in acts_of_the_day:
-        act.validation_locked = True
-        act.save()
-    if service.name == 'CMPP':
+        if commit:
+            act.validation_locked = True
+            act.save()
+    if service.name == 'CMPP' and commit:
         for patient, _ in patients.items():
             patient.create_diag_healthcare(user)
             patient.automated_switch_state(user)
-    return (nb_acts_total, nb_acts_validated, nb_acts_absence, nb_acts_double)
+    return (nb_acts_total, nb_acts_validated, nb_acts_double,
+        nb_acts_abs_non_exc, nb_acts_abs_exc, nb_acts_annul_nous,
+        nb_acts_annul_famille, nb_acts_abs_ess_pps,
+        nb_acts_enf_hosp)
