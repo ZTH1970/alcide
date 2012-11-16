d7c741dbd2ff9c62a51220214d3e404e8046a14f
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2012-11-16 17:01:54 +0100

    actes: start implementing the acts listing view

--- calebasse/actes/urls.py
+++ calebasse/actes/urls.py
@@ -1,16 +1,10 @@
-from django.conf.urls import patterns, url
-from calebasse.cbv import ListView, CreateView, DeleteView, UpdateView
+from django.conf.urls import patterns, url, include
 
-from models import Act
-from forms import CreateActForm, EditActForm
+act_patterns = patterns('calebasse.actes.views',
+        url(r'^$', 'act_listing'),
+)
 
-urlpatterns = patterns('',
-    url(r'^$', ListView.as_view(model=Act)),
-    url(r'^nouveau/$', CreateView.as_view(model=Act,
-        form_class=CreateActForm,
-        template_name_suffix='_nouveau.html')),
-    url(r'^(?P<pk>\d+)/$', UpdateView.as_view(model=Act,
-        form_class=EditActForm,
-        template_name_suffix='_edit.html')),
-    url(r'^(?P<pk>\d+)/supprimer/$', DeleteView.as_view(model=Act)),
+urlpatterns = patterns('calebasse.actes.views',
+        url(r'^$', 'redirect_today'),
+        url(r'^(?P<date>[^/]*)/', include(act_patterns))
 )
--- calebasse/actes/views.py
+++ calebasse/actes/views.py
@@ -1,1 +1,23 @@
-# Create your views here.
+import datetime
+
+from django.shortcuts import redirect
+
+from calebasse.cbv import ListView
+
+import models
+
+def redirect_today(request, service):
+    '''If not date is given we redirect on the agenda for today'''
+    return redirect(act_listing, date=datetime.date.today().strftime('%Y-%m-%d'),
+            service=service)
+
+class ActListingView(ListView):
+    model=models.EventAct
+    template_name='actes/act_listing.html'
+
+    def get_queryset(self):
+        qs = super(ActListingView, self).get_queryset()
+        qs.filter(services=self.service)
+        return qs
+
+act_listing = ActListingView.as_view()
