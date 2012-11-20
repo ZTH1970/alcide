ad267a2f30428248dbb1c8bcdd46c7bbc3394dcc
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2012-11-20 15:36:03 +0100

    facturation: clean urls.py

--- calebasse/facturation/urls.py
+++ calebasse/facturation/urls.py
@@ -1,17 +1,6 @@
 from django.conf.urls import patterns, url
 
-from calebasse.cbv import ListView, CreateView, DeleteView, UpdateView
-
-from models import Invoice
-from forms import CreateInvoiceForm, EditInvoiceForm
+import views
 
 urlpatterns = patterns('',
-    url(r'^$', ListView.as_view(model=Invoice)),
-    url(r'^new/$', CreateView.as_view(model=Invoice,
-        form_class=CreateInvoiceForm,
-        template_name_suffix='_new.html')),
-    url(r'^(?P<pk>\d+)/$', UpdateView.as_view(model=Invoice,
-        form_class=EditInvoiceForm,
-        template_name_suffix='_edit.html')),
-    url(r'^(?P<pk>\d+)/supprimer/$', DeleteView.as_view(model=Invoice)),
 )
