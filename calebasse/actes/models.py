542692b3c0f64783b587cdf9943243fde5a7b745
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2012-11-21 15:31:41 +0100

    actes: interpolation of unicode string in a non unicode string in eventact __unicode__ method, fixes #1997

--- calebasse/actes/models.py
+++ calebasse/actes/models.py
@@ -321,14 +321,14 @@ class EventAct(Act, Event):
             verbose_name=u'Convoqué')
 
     def __unicode__(self):
-        return 'Rdv le {0} de {1} avec {2} pour {3}'.format(
+        return u'Rdv le {0} de {1} avec {2} pour {3}'.format(
                 self.occurrence_set.all()[0].start_time, self.patient,
                 ', '.join(map(unicode, self.participants.all())),
                 self.act_type)
 
     def __repr__(self):
-        return '<%s %r %r>' % (self.__class__.__name__, unicode(self),
-            self.id)
+        return (u'<%s %r %r>' % (self.__class__.__name__, unicode(self),
+            self.id)).encode('utf-8')
 
     def start_time(self):
         return self.occurrence_set.all()[0].start_time
