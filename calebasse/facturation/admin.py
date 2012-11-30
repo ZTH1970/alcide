070e274aa7bae2bf05d924214e9d45e9f0e2da8f
Mikaël Ates <mates@entrouvert.com>
2012-11-30 13:13:15 +0100

    facturation: Add PricePerAct to admin.

--- calebasse/facturation/admin.py
+++ calebasse/facturation/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 import reversion
 
-from calebasse.facturation.models import Invoicing
+from calebasse.facturation.models import Invoicing, PricePerAct
 
 admin.site.register(Invoicing, reversion.VersionAdmin)
+admin.site.register(PricePerAct, reversion.VersionAdmin)
