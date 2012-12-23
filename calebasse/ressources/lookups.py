caf6b116f56ec9b8896a0ba09fc85d2cc3a95567
Frédéric Péters <fpeters@entrouvert.com>
2012-12-23 22:09:41 +0100

    agenda: add possibility to add all workers from a service (#1995)

--- calebasse/ressources/lookups.py
+++ calebasse/ressources/lookups.py
@@ -1,8 +1,39 @@
+import itertools
 
 from ajax_select import LookupChannel
 from calebasse.personnes.models import Worker
+from calebasse.ressources.models import Service
+
+class FakeGroup:
+    pk = None
+    label = None
+
+    def __init__(self, pk, label):
+        self.pk = pk
+        self.label = label
+
+    def __unicode__(self):
+        return u'Groupe: %s' % self.label
+
 
 class WorkerOrGroupLookup(LookupChannel):
     model = Worker
     search_field = 'display_name'
-    # TODO: also look for groups
+
+    def get_query(self, q, request):
+        service = None
+        if q.startswith('group:'):
+            service = Service.objects.get(pk=int(q[6:]))
+        else:
+            try:
+                service = Service.objects.get(name__iexact=q)
+            except Service.DoesNotExist:
+                pass
+
+        if service:
+            kwargs = { "%s__icontains" % self.search_field : q }
+            group = FakeGroup('group:%s' % service.id, service.name)
+            return itertools.chain([group], self.model.objects.for_service(service.id).order_by(self.search_field))
+
+        kwargs = { "%s__icontains" % self.search_field : q }
+        return self.model.objects.filter(enabled=True).filter(**kwargs).order_by(self.search_field)
