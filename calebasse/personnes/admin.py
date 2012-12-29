7d0d13bc46f8aef8709cc189cd54ef2b6f167977
Mikaël Ates <mates@entrouvert.com>
2012-12-29 17:01:18 +0100

    personnes: add externs to admin.

--- calebasse/personnes/admin.py
+++ calebasse/personnes/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 import reversion
 
 from calebasse.personnes.models import (Holiday, People, TimeTable,
-        SchoolTeacher, UserWorker, Worker, Role)
+        SchoolTeacher, UserWorker, Worker, ExternalWorker,
+        ExternalTherapist, Role)
 
 admin.site.register(Holiday, reversion.VersionAdmin)
 admin.site.register(People, reversion.VersionAdmin)
@@ -11,4 +12,6 @@ admin.site.register(TimeTable, reversion.VersionAdmin)
 admin.site.register(SchoolTeacher, reversion.VersionAdmin)
 admin.site.register(UserWorker, reversion.VersionAdmin)
 admin.site.register(Worker, reversion.VersionAdmin)
+admin.site.register(ExternalWorker, reversion.VersionAdmin)
+admin.site.register(ExternalTherapist, reversion.VersionAdmin)
 admin.site.register(Role, reversion.VersionAdmin)
