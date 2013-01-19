662daf2402e6bd795b78d0cae53301e12a139ff7
Mikaël Ates <mates@entrouvert.com>
2013-01-19 15:03:15 +0100

    scripts: update search acts imported multiple times.

--- scripts/check_double.py
+++ scripts/check_double.py
@@ -17,12 +17,21 @@ def main():
                 same_acts = Act.objects.filter(patient=patient, date=act.date, time=act.time)
                 nb = same_acts.count()
                 if nb > 1:
+                    keep = None
+                    for a in same_actes:
+                        state = a.get_state()
+                        if state and state.name != 'NON_VALIDE' and a.validation_locked = =True:
+                            keep = a
+                            break
+                    if not keep:
+                        lockeds = same_acts.filter(validation_locked=True)
+                        if lockeds.count() >= 1:
+                            keep = lockeds[0]
+                        else:
+                            keep = same_actes[0]
+                    same_acts.exclude(pk=keep.pk).delete()
                     i += 1
-                    print nb
-                    j = 0
-                    print "Valide : %d" % same_acts.filter(valide=True).count()
-                    print "Locked : %d" % same_acts.filter(validation_locked=True).count()
-    print "Nb actes redondants : %d" % i
+    print "Nb actes redondants traites: %d" % i
 
 if __name__ == "__main__":
     main()
