128aa0a61c97ea01e18e2e1dfc6f84694c1913d7
Thomas NOEL <tnoel@entrouvert.com>
2013-02-06 17:00:09 +0100

    transmission_utils.py: build IRIS/B2 mail

--- calebasse/facturation/transmission_utils.py
+++ calebasse/facturation/transmission_utils.py
@@ -2,10 +2,13 @@
 
 import os
 import sys
+import re
+import base64
+from datetime import datetime
+import zlib
+
 import ldap
 from M2Crypto import X509, SSL, Rand, SMIME, BIO
-from datetime import datetime
-import base64
 
 LDAP_HOST = 'ldap://annuaire.gip-cps.fr'
 LDAP_BASEDN = 'o=gip-cps,c=fr'
@@ -16,7 +19,21 @@ LDAP_CA_ATTRS = {
         'crl': ('certificateRevocationList;binary', 'X509 CRL'),
         'delta-crl': ('deltaRevocationList;binary', 'X509 CRL'),
     }
-CAPATH = '/var/lib/calebasse/gie-cps.capath/'
+
+CAPATH = '/var/lib/calebasse/gip-cps.capath/'
+
+RANDFILE = '/var/tmp/randpool.dat'
+
+MAILPATH = '/var/lib/calebasse/mail.out/'
+SENDER = 'teletransmission@aps42.org'
+MESSAGE_ID_RIGHT = 'teletransmission.aps42.org'
+VVVVVV = '100500'  # ETS-DT-001-TransportsFlux_SpecsTechCommune_v1.1.pdf
+NUMERO_EMETTEUR = '00000420788606'
+EXERCICE = NUMERO_EMETTEUR
+
+#
+# get a certificate from gip-cps LDAP
+#
 
 def get_certificate(large_regime, dest_organism):
     """
@@ -47,17 +64,24 @@ def get_certificate(large_regime, dest_organism):
         now = datetime.utcnow().replace(tzinfo=None)
         if startdate >= now >= enddate:
             # TODO : add capath + crl validation
-            # CAPATH = '/tmp/gie-cps.capath/'
+            # CAPATH = '/tmp/gip-cps.capath/'
             # os.execute(openssl verify -CApath CAPATH -crl_check cert.pem)
             certificates[serial] = x509
     if certificates:
         return certificates[max(certificates)]
     return None
 
-def smime_payload(message, x509, randfile=None):
+#
+# IRIS/B2 mail construction
+#
+
+def smime_payload(message, x509, randfile=RANDFILE):
     """
-    output s/mime message (headers+payload), encrypted with x509 certificate
+    output s/mime message (headers+payload), compressed & encrypted with x509 certificate
     """
+    # compress
+    zmessage = zlib.compress(message)
+    # encrypt
     if randfile:
         Rand.load_file(randfile, -1)
     s = SMIME.SMIME()
@@ -65,14 +89,53 @@ def smime_payload(message, x509, randfile=None):
     sk.push(x509)
     s.set_x509_stack(sk)
     s.set_cipher(SMIME.Cipher('des_ede3_cbc'))
-    bio = BIO.MemoryBuffer(message)
+    bio = BIO.MemoryBuffer(zmessage)
     pkcs7 = s.encrypt(bio)
-    print dir(pkcs7)
     out = BIO.MemoryBuffer()
     s.write(out, pkcs7)
     if randfile:
         Rand.save_file(randfile)
     return out.read()
+
+def build_mail(large_regime, dest_organism, b2, test=True):
+    """
+    build a mail to healt center, with b2-encrypted information
+    """
+    now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    n = 0
+    while n < 99999:
+        n += 1
+        message_id = '%s%0.5d' % (now, n)
+        filename = os.path.join(MAILPATH, message_id)
+        if not os.path.exists(filename):
+            break
+    else:
+        raise Exception('too much mails with prefix %s in %s' % (now, MAILPATH))
+    # count invoice in the b2 file = lines start with "5"
+    nb_invoices = len(re.findall('^5', b2, re.MULTILINE))
+    subject = 'CP%s/%s/%s/%0.5d' % (VVVVVV, EXERCICE, message_id, nb_invoices)
+    mail = {
+            'From': SENDER,
+            'To': large_regime + dest_organism + '@' + dest_organism + '.' + large_regime + '.rss.fr',
+            'Message-ID': '<%s@%s>' % (message_id, MESSAGE_ID_RIGHT),
+            'Subject': subject,
+            }
+    if test:
+        mail['Content-Description'] = 'CPTEST/B2/Z'
+    else:
+        mail['Content-Description'] = 'CP/B2/Z'
+    x509 = get_certificate(large_regime, dest_organism)
+    smime = smime_payload(b2, x509)
+
+    fd = open(filename, 'w')
+    for k,v in mail.items():
+        fd.write('%s: %s\n' % (k,v))
+    fd.write(smime)
+    fd.close()
+
+#
+# CApath construction
+#
 
 def der2pem(der, type_='CERTIFICATE'):
     return "-----BEGIN %s-----\n%s\n-----END %s-----\n" % \
@@ -80,8 +143,8 @@ def der2pem(der, type_='CERTIFICATE'):
 
 def build_capath(path):
     """
-    get all pkiCA from the gie-cps.fr ldap, store them in path
-    note: the gie-cps.fr ldap is limited to 10 objects in a response... by chance, there is less than 10 pkiCA ;)
+    get all pkiCA from the gip-cps.fr ldap, store them in path
+    note: the gip-cps.fr ldap is limited to 10 objects in a response... by chance, there is less than 10 pkiCA ;)
     """
     l = ldap.initialize(LDAP_HOST)
     results = l.search_s(LDAP_BASEDN,ldap.SCOPE_SUBTREE,'(objectclass=pkiCA)')
@@ -97,6 +160,8 @@ def build_capath(path):
                     fd.close()
 
 if __name__ == '__main__':
+    build_mail('01','422','000....b2-power')
+    sys.exit(0)
     if len(sys.argv) > 1:
         capath = sys.argv[1]
     else:
