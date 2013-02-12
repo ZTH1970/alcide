698d6bd59f810f3527c663a9f51362c018f6f394
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2013-02-12 13:38:44 +0100

    settings: add french formatting for numbers

--- calebasse/settings/formats/fr/formats.py
+++ calebasse/settings/formats/fr/formats.py
@@ -2,3 +2,6 @@ DATE_FORMAT = 'l d F Y'
 SHORT_DATE_FORMAT = 'j/n/Y'
 DATE_INPUT_FORMATS = ('%d/%m/%Y', '%d/%m/%Y', '%Y-%m-d')
 TIME_INPUT_FORMATS = ( '%Hh%M', '%H:%M', '%H%M', '%Hh' )
+DECIMAL_SEPARATOR = ','
+NUMBER_GROUPING = 3
+THOUSAND_SEPARATOR = ' '
