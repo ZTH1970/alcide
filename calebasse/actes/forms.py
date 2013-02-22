36dd6ab268b9b177037d7ad06b42d7802c1a2f6a
Jérôme Schneider <jschneider@entrouvert.com>
2013-02-22 18:46:37 +0100

    actes: fix search and begin filters support

--- calebasse/actes/forms.py
+++ calebasse/actes/forms.py
@@ -4,12 +4,17 @@ from django import forms
 
 class ActSearchForm(forms.Form):
     STATES = (
+            ('valide', u'Validé'),
             ('non-invoicable', u'Non facturable'),
+            ('is-billable', u'Facturable'),
             ('absent-or-canceled', u'Absent ou annulés'),
             ('lost', u'Perdus'),
+            ('pause-invoicing', u'Pause facturation'),
             ('invoiced', u'Facturé'),
-            ('last-invoicing', u'Dernière facturation'),
-            ('current-invoicing', u'Facturation en cours'))
+            ('invoiced', u'Inversion de facturabilité'),
+#            ('last-invoicing', u'Dernière facturation'),
+            ('current-invoicing', u'Facturation en cours')
+            )
 
     INITIAL = [x[0] for x in STATES]
 
--- calebasse/actes/views.py
+++ calebasse/actes/views.py
@@ -19,47 +19,24 @@ class ActListingView(ListView):
 
     def get_queryset(self):
         qs = super(ActListingView, self).get_queryset()
-        #self.get_search_form()
-        self.search_form = forms.ActSearchForm()
         qs = qs.filter(patient__service=self.service)
         qs = qs.filter(date=self.date)
-        if self.search_form.is_valid():
-            cd = self.search_form.cleaned_data
-            last_name = cd['last_name']
-            if last_name:
-                qs = qs.filter(patient__last_name__icontains=last_name)
-            patient_record_id = cd['patient_record_id']
-            if patient_record_id:
-                qs = qs.filter(patient__id=int(patient_record_id))
-            social_security_number = cd['social_security_number']
-            # FIXME: what to do with the ssn ?
-            doctor_name = cd['doctor_name']
-            if doctor_name:
-                qs = qs.filter(doctors__last_name__icontains=doctor_name)
-#            filters = cd['filters']
-#            if 'non-invoicable' in filters:
-#                pass # FIXME
-#            if 'absent-or-canceled' in filters:
-#                pass # FIXME
-#            if 'lost' in filters:
-#                pass # FIXME
-#            if 'invoiced' in filters:
-#                pass # FIXME
-#            if 'last-invoicing' in filters:
-#                pass # FIXME
-#            if 'current-invoicing' in filters:
-#                pass # FIXME
+        self.search_form = forms.ActSearchForm(data=self.request.GET or None)
+        last_name = self.request.GET.get('last_name')
+        patient_record_id = self.request.GET.get('patient_record_id')
+        social_security_number = self.request.GET.get('social_security_number')
+        doctor_name = self.request.GET.get('doctor_name')
+        filters = self.request.GET.getlist('filters')
+        if last_name:
+            print last_name
+            qs = qs.filter(patient__last_name__istartswith=last_name)
+        if patient_record_id:
+            qs = qs.filter(patient__id=int(patient_record_id))
+        if doctor_name:
+            qs = qs.filter(doctors__last_name__icontains=doctor_name)
+        if 'lost' in filters:
+            qs = qs.filter(is_lost=True)
         return qs
-
-    #def get_search_form(self):
-    #    if self.request.method == 'POST':
-    #        self.search_form = forms.ActSearchForm(data=self.request.POST)
-    #    else:
-    #        self.search_form = forms.ActSearchForm()
-    #    return self.search_form
-
-   # def post(self, *args, **kwargs):
-   #     return self.get(*args, **kwargs)
 
     def get_context_data(self, **kwargs):
         ctx = super(ActListingView, self).get_context_data(**kwargs)
