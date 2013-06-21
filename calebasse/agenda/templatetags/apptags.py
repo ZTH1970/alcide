208d52bb9c306dff37bd102a2a17807fefa40c82
Mikaël Ates <mates@entrouvert.com>
2013-06-21 11:01:12 +0200

    agenda: add some filters.

--- calebasse/agenda/templatetags/apptags.py
+++ calebasse/agenda/templatetags/apptags.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django import template
 
 register = template.Library()
@@ -13,3 +14,21 @@ def is_worker_in_service(worker, service_name):
 @register.filter
 def is_intervenant(worker):
     return worker.type.intervene
+
+
+@register.filter
+def str_length_lt(value, arg):
+    value = str(value)
+    return len(value) < int(arg)
+
+@register.filter
+def trunc_act_type(value):
+    value = value.replace('PSYCHOMOTRICITE', 'PSYCHOMOT.')
+    value = value.replace('PSYCHOMOTRICITÉ', 'PSYCHOMOT.')
+    value = value.replace('GROUPE', 'GR.')
+    value = value.replace('BILAN', 'BIL.')
+    value = value.replace('THERAPIE', 'THER.')
+    value = value.replace('THÉRAPIE', 'THER.')
+    if len(value) > 24:
+        return value[:24] + '.'
+    return value
