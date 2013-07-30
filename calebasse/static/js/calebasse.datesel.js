c4ef26cbd62f8e5e99baf6c3d2b52f303e4c0369
Serghei MIHAI <smihai@entrouvert.com>
2013-07-30 14:07:02 +0200

    agenda: week number displayed next to the selected date
    Closes #3113x

--- calebasse/static/js/calebasse.datesel.js
+++ calebasse/static/js/calebasse.datesel.js
@@ -8,10 +8,15 @@
         }
     });
     $('#date-selector').on('change', function () {
-        var date = $(this).datepicker('getDate')
+        var date = $(this).datepicker('getDate');
         var year = date.getFullYear();
         var month = date.getMonth() + 1;
+        var first_jan = new Date(year, 0, 1);
+        var dayOfYear = ((date - first_jan + 1) / 86400000);
+        var week = Math.ceil(dayOfYear/7);
         var day = date.getDate();
+        var current_date = $(this).attr('value');
+        $(this).attr('value', current_date + ' (' + week + ')');
         var url_tpl = $(this).data('url');
         var new_date = year + '-' + month + '-' + day;
         var url = url_tpl.replace(/[0-9]{4}-[0-9]{2}-[0-9]{2}/, new_date);
