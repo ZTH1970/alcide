ZTH1970/alcide

commit 40e2065eba750ef1651b66eea2631d6c60506858

agenda: Fixing the current displayed date in datepicker
Closes #3433

calebasse/static/js/calebasse.datesel.js
@@ -1,18 +1,11 @@
 (function($) {
   $(function () {
+    var value = $('#date-selector').attr('value');
+    var week = value.substring(value.indexOf('(') + 1, value.length - 1);
+    var format = "DD d MM yy (" + week + ")";
     $('#date-selector').datepicker({
-        dateFormat: "DD d MM yy",
+        dateFormat: format,
         showWeek: true,
-        beforeShowDay: function(date) {
-            var selected_date = location.pathname.split('/')[3];
-            var year = date.getFullYear();
-            var month = date.getMonth() + 1;
-            var day = date.getDate();
-            var possible_current = [year + '-' + month + '-' + day,
-                                    year + (month < 10? '-0': '-') + month +
-                                    (day < 10? '-0': '-') + day];
-            return [true, $.inArray(selected_date, possible_current) >= 0 ? 'active-cell': 'unactive-cell']
-        },
         onClose: function(dateText, inst) {
             console.log('close');
         }
@@ -26,7 +19,8 @@
         var week = Math.ceil(dayOfYear/7);
         var day = date.getDate();
         var current_date = $(this).attr('value');
-        $(this).attr('value', current_date + ' (' + week + ')');
+        current_date = current_date.replace(/\([0-9]{2}\)/, '(' + week + ')');
+        $(this).attr('value', current_date);
         var url_tpl = $(this).data('url');
         var new_date = year + '-' + month + '-' + day;
         var url = url_tpl.replace(/[0-9]{4}-[0-9]{2}-[0-9]{2}/, new_date);

commit c78cce47f931863fac9b9b4caf27e90681e669f1

Regression introduced by the commit 40e2065 fixed

calebasse/static/js/calebasse.datesel.js
@@ -1,8 +1,13 @@
 (function($) {
   $(function () {
-    var value = $('#date-selector').attr('value');
-    var week = value.substring(value.indexOf('(') + 1, value.length - 1);
-    var format = "DD d MM yy (" + week + ")";
+    try {
+        // in case of the #date-selector element doesn't exist
+        var value = $('#date-selector').attr('value');
+        var week = value.substring(value.indexOf('(') + 1, value.length - 1);
+        var format = "DD d MM yy (" + week + ")";
+    } catch(err) {
+        var format = "DD d MM yy";
+    };
     $('#date-selector').datepicker({
         dateFormat: format,
         showWeek: true,