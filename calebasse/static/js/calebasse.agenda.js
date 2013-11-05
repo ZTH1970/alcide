2a72f99fb2e0dc44c63d763d7ca9dd41d1c2692e
Serghei MIHAI <smihai@entrouvert.com>
2013-11-05 10:57:25 +0100

    agenda: initializing the participants list when no cookies set yet

--- calebasse/static/js/calebasse.agenda.js
+++ calebasse/static/js/calebasse.agenda.js
@@ -86,7 +86,10 @@ function enable_events(base) {
         return false;
       });
       $(base).find('.newrdv').click(function() {
-          var participants = $.cookie('agenda-worker-tabs').map(function(i, v) { var data = i.split('-'); return data[2]});
+          var participants = new Array();
+          if ($.cookie('agenda-worker-tabs')) {
+              participants = $.cookie('agenda-worker-tabs').map(function(i, v) { var data = i.split('-'); return data[2]});
+          }
           var qs = $.param({participants: $.makeArray(participants),
                             room: $.cookie('active-ressource-agenda'),
                             time: $(this).data('hour') }, true);
