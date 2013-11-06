399f783463da88630c5c5f00f100b1eaede24045
Mikaël Ates <mates@entrouvert.com>
2013-11-06 09:28:46 +0100

    statistics: refine active patients and patient per worker stats (bis).

--- calebasse/statistics/forms.py
+++ calebasse/statistics/forms.py
@@ -14,8 +14,12 @@ class StatForm(Form):
     patients = make_ajax_field(Statistic, 'patients', 'patientrecord', True)
     participants = make_ajax_field(Statistic, 'participants', 'worker-or-group', True)
 
+class ActivePatientsForm(Form):
+    display_or_export = forms.BooleanField(label=u'Exporter dans un fichier', required=False, localize=True)
+    start_date = forms.DateField(label=u'Date de début', required=False, localize=True)
+    end_date = forms.DateField(label=u'Date de fin', required=False, localize=True)
 
-class AnnualStatForm(Form):
+class AnnualActivityForm(Form):
     display_or_export = forms.BooleanField(label=u'Exporter dans un fichier', required=False, localize=True)
     start_date = forms.DateField(label=u"Date de l'année souhaitée", required=False, localize=True)
     participants = make_ajax_field(Statistic, 'participants', 'worker-or-group', True,
