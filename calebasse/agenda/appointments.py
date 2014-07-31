af0ca53c51777441e28170c23d8fcc1fa628c5c5
Mikaël Ates <mates@entrouvert.com>
2014-07-31 23:51:51 +0200

    agenda: add already_billed attribute to appointment.

--- calebasse/agenda/appointments.py
+++ calebasse/agenda/appointments.py
@@ -85,6 +85,7 @@ class Appointment(object):
             self.patient_record_paper_id = event.patient.paper_id
             self.act_type = event.act_type.name
             self.is_billed = event.act.is_billed
+            self.already_billed = event.act.already_billed
             state = event.get_state()
             state_name = state.state_name if state else 'NON_VALIDE'
             display_name = VALIDATION_STATES[state_name]
