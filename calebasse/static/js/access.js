73afaff31dda2c8feaf433cda8c417506e7e96f6
Serghei MIHAI <smihai@entrouvert.com>
2014-10-03 16:43:51 +0200

    personnes: do not empty email field on user update form

--- calebasse/static/js/access.js
+++ calebasse/static/js/access.js
@@ -1,6 +1,6 @@
 (function($){
     $(function() {
-        $.each(['email', 'password1', 'password2'], function(i, element) {
+        $.each(['password1', 'password2'], function(i, element) {
             $('form input[name=' + element + ']').val('');
         })
     })
