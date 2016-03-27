720ce02d052f90fc37f0e8262476cb9464774d77
ZTH1970 <7577thierry@gmail.com>
2016-03-27 22:52:02 +0200

    Complement descriptif installation.

--- alcide/local_settings_example_mysql.py
+++ alcide/local_settings_example_mysql.py
@@ -3,9 +3,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'alcide',
-        'USER': 'root',
-        'PASSWORD': 'admin',
-        'HOST': '192.168.0.24',   # Or an IP Address that your DB is hosted on
+        'USER': 'MyUser',
+        'PASSWORD': 'MyPassword',
+        'HOST': 'IpSereur',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
 }
